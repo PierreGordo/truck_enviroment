cff077fd04987e027be354c253e3bc5ded920d78
PierreGordo <peter.gardas@gmail.com>
2025-05-02 18:35:46 +0200

    Started work on rendering points.

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,8 @@ fn main() {
     .add_plugins(DefaultPlugins
         //This part fixes weird white edges when importing sprite images
         .set(ImagePlugin::default_nearest()))
-    .add_systems(Startup, (setup, render_platform, render_background))
-    .add_systems(Update, (camera_player_lock, move_player, zoom_camera))
+    .add_systems(Startup, (setup, spawn_truck, spawn_point))
+    .add_systems(Update, (camera_player_lock, move_truck, zoom_camera))
     .run();
 }
 
@@ -29,26 +29,30 @@ fn setup(mut commands: Commands){
     //Spawn camera so the stuff actually shows up
     commands.spawn((Camera2d::default(), 
                     Camera));
+    //Render background
+    //At first a solid colour, then an image
+    commands.spawn((Sprite::from_color ( 
+        Color::srgb(0.0, 0.8, 0.0),
+        Vec2::new(2000.0, 2000.0),
+         ),
+        //Z here is used for ordering (so this will be above elements with 0 z etc..)
+        Transform::from_xyz(0.0, 0.0, 0.0)
+        ));
 }
 
-fn render_platform(mut commands: Commands, asset_server: Res<AssetServer>){
+fn spawn_truck(mut commands: Commands, asset_server: Res<AssetServer>){
 
     commands.spawn((Sprite::from_image(asset_server.load("military_truck_above.png")),
                     //Doing a simple mark so I can query this sprite later as my player
                     Player));
 }
 
-//Rendering background
-fn render_background(mut commands: Commands){
-    //At first a solid colour, then an image
-    commands.spawn((Sprite::from_color ( 
-                    Color::srgb(0.0, 0.8, 0.0),
-                    Vec2::new(2000.0, 2000.0),
-                     ),
-                    //Z here is used for ordering (so this will be above elements with 0 z etc..)
-                    Transform::from_xyz(0.0, 0.0, 0.0)
-                    ));
+//Function to visualise a point on given coords
+//A point is a text and a little circle to signify its position
+fn spawn_point(mut commands: Commands, mut mesh: ResMut<Assets<Mesh>>, mut material: ResMut<Assets<ColorMaterial>>){                                                        //The 5.0 value in transform is there for the ordering (so its visible)
+    commands.spawn((Mesh2d(mesh.add(Circle::new(15.0))), MeshMaterial2d(material.add(Color::hsl(0.9, 0.9, 0.0))), Transform::from_xyz(0.0, 0.0, 5.0)));
 }
+
 
 
 //System to move the camera slowly to test camera movement
@@ -65,7 +69,7 @@ fn camera_player_lock(mut camera_position: Query<&mut Transform, With<Camera>>, 
 
 
 //Move the player
-fn move_player(time: Res<Time>, keys: Res<ButtonInput<KeyCode>>, mut player_position: Query<&mut Transform, With<Player>>){
+fn move_truck(time: Res<Time>, keys: Res<ButtonInput<KeyCode>>, mut player_position: Query<&mut Transform, With<Player>>){
 
     //Speed of movement
     let speed = 500.0;
@@ -116,5 +120,4 @@ fn zoom_camera(mouse_wheel: Res<AccumulatedMouseScroll>, camera_query: Single<&m
             orthographic.scale = 1.0;
         }
     }
-
 }
